9601f64f676ea6fc224b8aa59bcd30f9f9db71c6
Justin Tu <coppolavina@gmail.com>
2022-01-09 03:28:06 +0000

    count number of products, categories and add About us

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV.username, password: ENV.password
+  http_basic_authenticate_with name: ENV["username"], password: ENV["password"]
+
   def show
+    @number_of_products = Product.count(:name)
+    @number_of_categories = Category.count(:name)
   end
 end
