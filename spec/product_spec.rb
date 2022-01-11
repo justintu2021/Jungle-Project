e360c3c77579baf2b934cc6fb814005ae056b76f
Justin Tu <coppolavina@gmail.com>
2022-01-11 19:19:32 +0000

    test product

---
+++ spec/product_spec.rb
@@ -0,0 +1,24 @@
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
+RSpec.describe Product, type: :model do
+  describe 'Validation' do
+    
+    it "return nil if we don't input name" do
+      @category = Category.new(name: "fashion")
+      @product = Product.new(description: "men",image: open_asset('apparel1.jpg'), price_cents: 64.99, quantity: 3)
+      get_name = @product.name
+
+      expect(get_name).to be_nil
+    end
+
+    it "return nil if we don't input description" do
+      @category = Category.new(name: "fashion")
+      @product = Product.new(name: "highlight",image: open_asset('apparel1.jpg'), price_cents: 64.99, quantity: 3)
+      get_description = @product.description
+
+      expect(get_description).not_to be_nil
+    end
+  end
+end
